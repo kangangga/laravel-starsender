eb39dc3ec7fca55b8eb3267643df789833595aa1
Angga Saputra <masangga.com@gmail.com>
2022-05-21 21:22:39 +0700

    test

---
+++ tests/UnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kangangga\Starsender\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+
+class UnitTest extends TestCase
+{
+    public function test_basic_test()
+    {
+        $this->assertTrue(true);
+    }
+}
